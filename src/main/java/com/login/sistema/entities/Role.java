4a2bbfbf3576114136de6ac4fd6e76400f3b2e02
aglayrton <aglayrtonjuliao@gmail.com>
2023-01-15 18:09:40 -0300

    Entities User e Role created

--- src/main/java/com/login/sistema/entities/Role.java
+++ src/main/java/com/login/sistema/entities/Role.java
@@ -1,5 +1,59 @@
 package com.login.sistema.entities;
 
+import java.util.UUID;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
 public class Role {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private UUID uid;
+  private String name;
+  private Integer type;
+  private Boolean enable_boot;
+
+  public Role() {
+  }
+
+  public Role(UUID uid, String name, Integer type, Boolean enable_boot) {
+    this.uid = uid;
+    this.name = name;
+    this.type = type;
+    this.enable_boot = enable_boot;
+  }
+
+  public UUID getUid() {
+    return uid;
+  }
+
+  public void setUid(UUID uid) {
+    this.uid = uid;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getType() {
+    return type;
+  }
+
+  public void setType(Integer type) {
+    this.type = type;
+  }
+
+  public Boolean getEnable_boot() {
+    return enable_boot;
+  }
+
+  public void setEnable_boot(Boolean enable_boot) {
+    this.enable_boot = enable_boot;
+  }
 
 }
--- src/main/java/com/login/sistema/entities/User.java
+++ src/main/java/com/login/sistema/entities/User.java
@@ -1,5 +1,72 @@
 package com.login.sistema.entities;
 
+import java.util.UUID;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private UUID uid;
+  private String email;
+  private String password;
+  private Boolean enable_boot;
+
+  @OneToOne()
+  @JoinColumn(name = "role_uid")
+  public Role role;
+
+  public User() {
+  }
+
+  public User(UUID uid, String email, String password, Boolean enable_boot, Role role) {
+    this.uid = uid;
+    this.email = email;
+    this.password = password;
+    this.enable_boot = enable_boot;
+    this.role = role;
+  }
+
+  public UUID getUid() {
+    return uid;
+  }
+
+  public void setUid(UUID uid) {
+    this.uid = uid;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Boolean getEnable_boot() {
+    return enable_boot;
+  }
+
+  public void setEnable_boot(Boolean enable_boot) {
+    this.enable_boot = enable_boot;
+  }
+
+  public Role getRole() {
+    return role;
+  }
+
+  public void setRole(Role role) {
+    this.role = role;
+  }
 
 }
