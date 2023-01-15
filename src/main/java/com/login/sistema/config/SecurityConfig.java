6135734758a099ee2d6e511f0697e0284d2fe587
aglayrton <aglayrtonjuliao@gmail.com>
2023-01-15 18:24:35 -0300

    Implements configuration security basic hardcore

---
+++ src/main/java/com/login/sistema/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.login.sistema.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    http.authorizeHttpRequests(
+      authorizeConfig->{
+        authorizeConfig.requestMatchers("/public", "/logout").permitAll();
+        authorizeConfig.anyRequest().authenticated();
+      }
+    ).formLogin()
+    .and()
+    .httpBasic()
+    .and()
+    .csrf().disable();
+
+
+    return http.build();
+
+  }
+
+  @Autowired
+  public void configure(AuthenticationManagerBuilder auth) throws Exception{
+    auth.inMemoryAuthentication()
+    .withUser("juliao")
+    .password("{noop}juliao")
+    .roles("ADMIN");
+  }
+
+}
