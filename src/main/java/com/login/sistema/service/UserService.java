d1ba651c3603d9b1071a78a10b5cf8ca899efffe
aglayrton <aglayrtonjuliao@gmail.com>
2023-01-15 23:04:27 -0300

    Add implements UserDetailsService

---
+++ src/main/java/com/login/sistema/service/UserService.java
@@ -0,0 +1,29 @@
+package com.login.sistema.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.login.sistema.entities.User;
+import com.login.sistema.repositories.UserRepository;
+
+@Service
+@Transactional
+public class UserService implements UserDetailsService {
+
+  @Autowired
+  UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByEmail(username);
+    if (user == null) {
+      throw new UsernameNotFoundException("invalid username");
+    }
+    return user;
+  }
+
+}
