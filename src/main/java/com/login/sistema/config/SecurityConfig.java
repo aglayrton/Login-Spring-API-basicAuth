ca68d0e1c70cad9e6137d932c343677e17e91046
aglayrton <aglayrtonjuliao@gmail.com>
2023-01-15 23:03:06 -0300

    auth basic by database

--- src/main/java/com/login/sistema/config/SecurityConfig.java
+++ src/main/java/com/login/sistema/config/SecurityConfig.java
@@ -1,11 +1,10 @@
 package com.login.sistema.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,12 +29,10 @@ public class SecurityConfig {
 
   }
 
-  @Autowired
-  public void configure(AuthenticationManagerBuilder auth) throws Exception{
-    auth.inMemoryAuthentication()
-    .withUser("juliao")
-    .password("{noop}juliao")
-    .roles("ADMIN");
+  @Bean
+  BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
+
 
 }
